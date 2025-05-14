df846b6d8ef10ecd2994d83730e3d2504c98a7fe
shashank.shekhar6 <shashank.shekhar6@172.18.137.52>
2025-05-14 14:19:32 +0530

    Files Added

---
+++ ElectricityBillSystem/src/com/shashank/billsystem/Billcalculator.java
@@ -0,0 +1,23 @@
+package com.shashank.billsystem;
+
+public class Billcalculator {
+
+	public static void main(String[] args) {
+		
+		int units = 280;
+		double billToPay = 0;
+		if(units < 100)
+		{
+			billToPay = units * 1.20;
+		}
+		else if(units < 300)
+		{
+			billToPay = 100 * 1.20 + (units - 100) * 2;
+		}
+		System.out.println("The electricity bill for" +units+ "is : " + billToPay);
+
+	}
+
+}
+
+
